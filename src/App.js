a3efc36074074fce10d736b763881acec9461775
AlistairFernandes <73463102+AlistairFernandes@users.noreply.github.com>
2021-09-03 14:41:25 +0530

    gdsa

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={InputPage} />
-        <Route exact path="/quotes" component={QuotePage} />
+        <Route exact path="/quotes/:groupCode" component={QuotePage} />
       </Switch>
     </Router>
   );
