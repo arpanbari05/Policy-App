d3e0be1b2f5e10dd33c5f89d3b950bc23c369e98
AlistairFernandes <73463102+AlistairFernandes@users.noreply.github.com>
2021-10-20 14:58:54 +0530

    edit member

--- src/pages/quotePage/components/BuyNowModal.jsx
+++ src/pages/quotePage/components/BuyNowModal.jsx
@@ -240,10 +240,11 @@ function BuyNowModalProduct({ groupCode, setShowBuyNow = () => {} }) {
               `}
               onClick={() => {
                 setShowBuyNow(false);
-                history.replace({
+                history.push({
                   pathname: `/quotes/${groupCode}`,
                   search: `enquiryId=${enquiryId}`,
                 });
+                dispatch(setSelectedGroup(groupCode));
               }}
             >
               <span>Add Plan</span> <img src={addBtn} alt="addbtn" />
