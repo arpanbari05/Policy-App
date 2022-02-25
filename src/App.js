b919be96360fc7de29d2c88ae1e96f1350852cae
Ronak singh <webronak2030@gmail.com>
2022-02-25 12:24:59 +0530

    404 error page and medical questions issues

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { lazy } from "react";
 import { Suspense } from "react";
 import { FullScreenLoader, LoadCart } from "./components";
 import ComparePage from "./pages/ComparePage";
-// import ErrorPage from "./components/Common/ErrorPage/ErrorPage";
+import ErrorPage from "./components/Common/ErrorPage/ErrorPage";
 
 const ProposalPage = lazy(() => import("./pages/ProposalPage/ProposalPage"));
 const ProposalSummary = lazy(() =>
@@ -50,9 +50,9 @@ function App() {
           <ThankYouPage />
         </LazyLoad>
       </Route>
-      {/* <Route path="*" component={ErrorPage} /> */}
+      <Route path="*" component={ErrorPage} />
 
-      <Route path="*" component={PageNotFound} />
+      {/* <Route path="*" component={PageNotFound} /> */}
       {process.env.NODE_ENV === "development" && (
         <Route path={"/test"} exact>
           Hi
