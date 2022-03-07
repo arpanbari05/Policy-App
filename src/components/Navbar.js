f4062769c0bea25b5eff2843482f3ba401b944cd
Rish7223 <rishabh.7223@gmail.com>
2022-03-07 14:10:23 +0530

    logo size fixed for mobile and desktop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Card from "./Card";
 import "styled-components/macro";
-import {
-  Link,
-  useLocation,
-  useParams,
-  useRouteMatch,
-  useHistory,
-} from "react-router-dom";
+import { Link, useLocation, useParams, useRouteMatch } from "react-router-dom";
 import ThemeModal from "./ThemeModal";
 import { useGetEnquiriesQuery } from "../api/api";
-import {
-  useFrontendBoot,
-  useMembers,
-  useTheme,
-  useUrlEnquiry,
-} from "../customHooks";
+import { useFrontendBoot, useMembers, useTheme } from "../customHooks";
 import { FaRegCopy } from "react-icons/fa";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  setPolicyTypes,
-  setPolicyType,
-  setIsOnProductDetails,
-} from "../pages/quotePage/quote.slice";
 import useComparePage from "../pages/ComparePage/useComparePage";
-import ShareButton from "./Common/Button/ShareButton";
-import { FaChevronLeft } from "react-icons/fa";
 import { CircleLoader } from ".";
 import ShareQuoteModal from "./ShareQuoteModal";
 import { useShareFunctionality } from "../customHooks";
-
-const GO_BACK_LOCATIONS = [
-  "/proposal",
-  "/proposal_summary",
-  "/quotes",
-  "/productdetails",
-];
 
 function LogoLink() {
   const {
@@ -54,9 +28,12 @@ function LogoLink() {
         alt={`companylogo`}
         css={`
           cursor: pointer;
-          max-height: 55px;
-          max-width: 130px;
+          max-width: 187px;
           object-fit: contain;
+
+          @media (max-width: 480px) {
+            max-width: 110px;
+          }
         `}
       />
     </Link>
@@ -66,11 +43,6 @@ function LogoLink() {
 const Navbar = () => {
   const location = useLocation();
 
-  const dispatch = useDispatch();
-
-  const history = useHistory();
-
-  const { getUrlWithEnquirySearch } = useUrlEnquiry();
   const isRootRoute = useRouteMatch({
     path: ["/", "/input/basic-details"],
     exact: true,
@@ -87,13 +59,6 @@ const Navbar = () => {
   const trace_id = data?.data?.trace_id;
 
   const { groupCode } = useParams();
-
-  const { getPreviousGroup, getLastGroup } = useMembers();
-
-  const lastGroup = getLastGroup();
-
-  const prevoiusGroup = getPreviousGroup(parseInt(groupCode));
-
   const { imageSend: proposalImageSend } = useShareFunctionality(
     "proposalPage",
     "proposalPage",
@@ -114,105 +79,14 @@ const Navbar = () => {
       `}
     >
       <Card width={"100%"} height={"55px"} clasName="position-relative">
-        {/* {location.pathname === "/proposal_summary" && (
-          <Link
-            className="d-flex justify-content-center align-items-center"
-            css={`
-              background: #f1f4f8;
-              width: 35px;
-              margin-right: 20px;
-              border-radius: 100%;
-              height: 35px;
-              top: 50%;
-              left: 20px;
-              transform: translateY(-50%);
-              position: absolute;
-              color: #707b8b;
-            `}
-            to={getUrlWithEnquirySearch("/proposal")}
-            //  onClick={() => {
-            //       history.push({ pathname: getUrlWithEnquirySearch("/proposal") });
-            //     }}
-          >
-            <FaChevronLeft />
-          </Link>
-        )} */}
-
         <div className="container d-flex justify-content-between align-items-center h-100">
           <div
             css={`
               display: flex;
               align-items: center;
               height: 100%;
-              /* padding: 0px 100px; */
             `}
           >
-            {/* {GO_BACK_LOCATIONS.filter(loc =>
-              location.pathname.startsWith(loc),
-            ) &&
-              !["/input/basic-details", "/input/journey-type"].includes(
-                location.pathname,
-              ) && (
-                <div
-                  className="d-flex justify-content-center align-items-center"
-                  css={`
-                    // background: ${colors.primary_color};
-                    width: 35px;
-                    margin-right: 5px;
-                    border-radius: 100%;
-                    height: 35px;
-                    color: ${colors.primary_color};
-                    cursor: pointer;
-                    &:hover {
-                      background: ${colors.primary_color}10;
-                    }
-                  `}
-                  onClick={() => {
-                    switch (location.pathname) {
-                      case `/productdetails/${groupCode}`:
-                        const getLink = () => {
-                          if (!prevoiusGroup) {
-                            return getUrlWithEnquirySearch(
-                              `/quotes/${groupCode}`,
-                            );
-                          }
-                          return getUrlWithEnquirySearch(
-                            `/productdetails/${prevoiusGroup.id}`,
-                          );
-                        };
-                        history.replace(getLink());
-                        setIsOnProductDetails(true);
-                        break;
-
-                      case "/proposal":
-                        // history.goBack();
-                        console.log("wvdskjvb", lastGroup);
-                        history.replace(
-                          getUrlWithEnquirySearch(
-                            `/productdetails/${lastGroup.id}`,
-                          ),
-                        );
-                        break;
-
-                      case "/proposal_summary":
-                        history.push(getUrlWithEnquirySearch("/proposal"));
-                        break;
-
-                      case `/quotes/${groupCode}`:
-                        history.replace(
-                          getUrlWithEnquirySearch(`/input/medicalHistory`),
-                        );
-                        return;
-
-                      default:
-                        history.goBack();
-                        return;
-                    }
-                  }}
-                >
-                  <FaChevronLeft />
-                </div>
-              )} */}
             <LogoLink />
             {!location.pathname.startsWith("/input") && trace_id && (
               <div
