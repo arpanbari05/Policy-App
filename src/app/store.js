e4b2eb9f59a02ff825a15c966027ee16aeb83ce6
Rish7223 <rishabh.7223@gmail.com>
2022-04-09 09:37:05 +0530

    minor fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,18 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "../api/api";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
 import rootReducer from "./reducers";
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const store = configureStore({
-  // reducer: persistedReducer,
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== "production",
   middleware: getDefaultMiddleware =>
@@ -22,7 +12,5 @@ const store = configureStore({
       immutableCheck: true,
     }).concat(api.middleware),
 });
-// let persistor = persistStore(store);
-// console.log("h32yg2332", store.getState());
-// export { store, persistor };
+
 export { store };
