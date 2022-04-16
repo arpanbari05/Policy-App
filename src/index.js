25ecaa9e8b7a220d19a50c448ac69602c06595c4
Rish7223 <rishabh.7223@gmail.com>
2022-04-16 18:18:03 +0530

    senetry removed

--- src/index.js
+++ src/index.js
@@ -4,18 +4,6 @@ import App from "./App";
 import AppProviders from "./AppProviders";
 import { GlobalStyles } from "./styles";
 import ErrorBoundary from "./ErrorBoundary";
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-
-// Sentry.init({
-//   dsn: "https://81abd1aa2c0140a8a30ed4c9cbe0570f@o1122045.ingest.sentry.io/6159259",
-//   integrations: [new BrowserTracing()],
-
-//   // Set tracesSampleRate to 1.0 to capture 100%
-//   // of transactions for performance monitoring.
-//   // We recommend adjusting this value in production
-//   tracesSampleRate: 1.0,
-// });
 
 ReactDOM.render(
   <React.StrictMode>
