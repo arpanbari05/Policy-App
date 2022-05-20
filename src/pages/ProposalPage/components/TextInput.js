0952f5802d1e16e7ee7014e73fd681d8165c33ba
arpanbari05 <arpanbari05@gmail.com>
2022-05-20 14:23:49 +0530

    Email limit to 64 chars & hiding 1 crore plan to POS user PINC broker

--- src/pages/ProposalPage/components/TextInput.js
+++ src/pages/ProposalPage/components/TextInput.js
@@ -206,6 +206,17 @@ const TextInput = ({
                 onChange(e);
                 setFallbackValue(e.target.value);
               }
+            } else if (checkValidation?.["matches"] === "email") {
+              if (
+                e.target.value.length <= 64 &&
+                checkAllChar(
+                  e.target.value,
+                  forbiddedSymbols.filter(el => !"@.".split("").includes(el)),
+                )
+              ) {
+                onChange(e);
+                setFallbackValue(e.target.value);
+              }
             } else {
               if (
                 notAllowed &&
