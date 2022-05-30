af54d19b81a94025835ea3e2cf2b9f1f6a2920ba
Rish7223 <rishabh.7223@gmail.com>
2022-05-30 09:39:34 +0530

    bugFix: ios select dropdown is not visible

--- src/pages/ComparePage/components/tenure/TenureMobile.js
+++ src/pages/ComparePage/components/tenure/TenureMobile.js
@@ -2,6 +2,8 @@ import { useGetDiscountsQuery } from "../../../../api/api";
 import { CircleLoader } from "../../../../components";
 import { useQuotesCompare } from "../../../../customHooks";
 import { FeatureValue } from "../../mobile/components";
+import "styled-components/macro";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const TenureFeatureValueMobile = ({ quote, groupCode, journeyType }) => {
   const { updateCompareQuote } = useQuotesCompare();
@@ -28,16 +30,35 @@ const TenureFeatureValueMobile = ({ quote, groupCode, journeyType }) => {
   return (
     <FeatureValue>
       {!isLoading ? (
-        <select
-          value={quote.tenure}
-          onChange={event => handleTenureChange(event.target.value)}
-        >
-          {data?.data?.map(value => (
-            <option key={value.tenure} value={value.tenure}>{`${value.tenure} ${
-              value.tenure === 1 ? "Year" : "Years"
-            }`}</option>
-          ))}
-        </select>
+        <>
+          <select
+            value={quote.tenure}
+            onChange={event => handleTenureChange(event.target.value)}
+            css={`
+              width: 60px;
+              background-color: transparent;
+              z-index: 10;
+              color: inherit;
+              -webkit-appearance: none;
+              &::-ms-expand {
+                display: none;
+              }
+            `}
+          >
+            {data?.data?.map(value => (
+              <option key={value.tenure} value={value.tenure}>{`${
+                value.tenure
+              } ${value.tenure === 1 ? "Year" : "Years"}`}</option>
+            ))}
+          </select>
+          <span
+            css={`
+              transform: translateX(-25px);
+            `}
+          >
+            <RiArrowDropDownLine size={25} />
+          </span>
+        </>
       ) : (
         <div>
           {quote.tenure} {quote.tenure === 1 ? "Year" : "Years"}
